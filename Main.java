2e88a5e520176a01b81c6e5a3451329c2fd286b0
saurabhchandavale <85121287+saurabhchandavale@users.noreply.github.com>
2022-05-26 11:58:23 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printIncresing(n);
+    }
+
+    public static void printIncresing(int n){
+        if(n == 0){
+            return;
+        }
+        printIncresing(n-1);
+        System.out.println(n);
+    }
+}
